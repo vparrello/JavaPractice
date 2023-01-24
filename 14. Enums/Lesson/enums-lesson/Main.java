f0b618d88958b208771f59149a61cff32235d8a8
Victoria Parrello <81109758+vparrello@users.noreply.github.com>
2023-01-24 09:18:39 -0600

    Add files via upload

---
+++ 14. Enums/Lesson/enums-lesson/Main.java
@@ -0,0 +1,11 @@
+import model.Car;
+import mode.Car.TrafficLight.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car("Nissan", 2020);
+        car.drive(RED);
+    }
+
+}
