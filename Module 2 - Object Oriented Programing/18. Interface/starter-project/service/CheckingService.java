34491c9532fd60456cd0cb7d71c71c219b4f3acf
Victoria Parrello <81109758+vparrello@users.noreply.github.com>
2023-01-24 09:29:52 -0600

    Add files via upload

---
+++ Module 2 - Object Oriented Programing/18. Interface/starter-project/service/CheckingService.java
@@ -0,0 +1,49 @@
+package service;
+
+import java.math.BigDecimal;
+
+import pojo.Checking;
+import repository.AccountRepository;
+
+public class CheckingService implements AccountService{
+        
+    AccountRepository accountRepository;
+
+    public CheckingService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+    
+    public void createAccount(Checking account) {
+        this.accountRepository.createAccount(account);
+    }
+
+    public Checking retrieveAccount(String id) {
+        return (Checking)this.accountRepository.retrieveAccount(id);
+    }
+
+    public void updateAccount(Checking account) {
+        this.accountRepository.updateAccount(account);
+    }
+
+    public void deleteAccount(String id) {
+        this.accountRepository.deleteAccount(id);
+    }
+
+    @Override
+    public void deposit(String id, BigDecimal amount) {
+        Checking checking = retrieveAccount(id);
+        checking.setBalance(checking.getBalance().add(amount));
+        updateAccount(checking);
+        
+    }
+
+    @Override
+    public void withdraw(String id, BigDecimal amount) {
+        Checking checking = retrieveAccount(id);
+        checking.setBalance(checking.getBalance().subtract(amount));
+        updateAccount(checking);
+        
+    }
+
+
+}
---
+++ Module 2 - Object Oriented Programing/18. Interface/starter-project/service/CreditService.java
@@ -0,0 +1,46 @@
+package service;
+
+import java.math.BigDecimal;
+
+import pojo.Credit;
+import repository.AccountRepository;
+
+public class CreditService implements AccountService {
+
+    AccountRepository accountRepository;
+
+    public CreditService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+    
+    public void createAccount(Credit account) {
+        this.accountRepository.createAccount(account);
+    }
+
+    public Credit retrieveAccount(String id) {
+        return (Credit)this.accountRepository.retrieveAccount(id);
+    }
+
+    public void updateAccount(Credit account) {
+        this.accountRepository.updateAccount(account);
+    }
+
+    public void deleteAccount(String id) {
+        this.accountRepository.deleteAccount(id);
+    }
+
+    @Override
+    public void deposit(String id, BigDecimal amount) {
+        Credit credit = retrieveAccount(id);
+        credit.setCredit(credit.getCredit().subtract(amount));
+        accountRepository.updateAccount(credit);
+    }
+    
+    @Override
+    public void withdraw(String id, BigDecimal amount) {
+        Credit credit = retrieveAccount(id);
+        credit.setCredit(credit.getCredit().add(amount));
+        accountRepository.updateAccount(credit);
+        
+    }
+}
